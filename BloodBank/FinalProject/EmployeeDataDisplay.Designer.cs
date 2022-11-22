49be6b979b97dee82287470e9fa7f67432512b13
Vincent Gonzalez <47186521+vinnyg723@users.noreply.github.com>
2022-11-22 13:27:04 -0600

    Renamed Files;

---
+++ BloodBank/FinalProject/EmployeeDataDisplay.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FinalProject
+{
+    partial class EmployeeDataDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmployeeDataDisplay));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearchDonors = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblOutputDonors = new System.Windows.Forms.Label();
+            this.chtBloodTypes = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblLowBlood = new System.Windows.Forms.Label();
+            this.btnDonateToday = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chtBloodTypes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(27, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search Donors:";
+            // 
+            // txtSearchDonors
+            // 
+            this.txtSearchDonors.Location = new System.Drawing.Point(154, 22);
+            this.txtSearchDonors.Name = "txtSearchDonors";
+            this.txtSearchDonors.Size = new System.Drawing.Size(119, 20);
+            this.txtSearchDonors.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.Red;
+            this.btnSearch.Location = new System.Drawing.Point(97, 57);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(101, 30);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblOutputDonors
+            // 
+            this.lblOutputDonors.AutoSize = true;
+            this.lblOutputDonors.BackColor = System.Drawing.Color.Transparent;
+            this.lblOutputDonors.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutputDonors.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblOutputDonors.Location = new System.Drawing.Point(27, 120);
+            this.lblOutputDonors.Name = "lblOutputDonors";
+            this.lblOutputDonors.Size = new System.Drawing.Size(0, 18);
+            this.lblOutputDonors.TabIndex = 3;
+            // 
+            // chtBloodTypes
+            // 
+            this.chtBloodTypes.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chtBloodTypes.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chtBloodTypes.Legends.Add(legend1);
+            this.chtBloodTypes.Location = new System.Drawing.Point(406, 9);
+            this.chtBloodTypes.Name = "chtBloodTypes";
+            this.chtBloodTypes.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            series1.BackSecondaryColor = System.Drawing.Color.Firebrick;
+            series1.BorderColor = System.Drawing.Color.Red;
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            series1.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.Legend = "Legend1";
+            series1.Name = "Blood";
+            this.chtBloodTypes.Series.Add(series1);
+            this.chtBloodTypes.Size = new System.Drawing.Size(438, 394);
+            this.chtBloodTypes.TabIndex = 4;
+            // 
+            // lblLowBlood
+            // 
+            this.lblLowBlood.AutoSize = true;
+            this.lblLowBlood.BackColor = System.Drawing.Color.Transparent;
+            this.lblLowBlood.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLowBlood.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblLowBlood.Location = new System.Drawing.Point(445, 390);
+            this.lblLowBlood.Name = "lblLowBlood";
+            this.lblLowBlood.Size = new System.Drawing.Size(0, 13);
+            this.lblLowBlood.TabIndex = 5;
+            // 
+            // btnDonateToday
+            // 
+            this.btnDonateToday.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDonateToday.ForeColor = System.Drawing.Color.Red;
+            this.btnDonateToday.Location = new System.Drawing.Point(543, 409);
+            this.btnDonateToday.Name = "btnDonateToday";
+            this.btnDonateToday.Size = new System.Drawing.Size(136, 30);
+            this.btnDonateToday.TabIndex = 6;
+            this.btnDonateToday.Text = "Donate Today?";
+            this.btnDonateToday.UseVisualStyleBackColor = true;
+            this.btnDonateToday.Click += new System.EventHandler(this.btnDonateToday_Click);
+            // 
+            // EmployeeDataDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(856, 530);
+            this.Controls.Add(this.btnDonateToday);
+            this.Controls.Add(this.lblLowBlood);
+            this.Controls.Add(this.chtBloodTypes);
+            this.Controls.Add(this.lblOutputDonors);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchDonors);
+            this.Controls.Add(this.label1);
+            this.Name = "EmployeeDataDisplay";
+            this.Text = "Donations";
+            ((System.ComponentModel.ISupportInitialize)(this.chtBloodTypes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearchDonors;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblOutputDonors;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtBloodTypes;
+        private System.Windows.Forms.Label lblLowBlood;
+        private System.Windows.Forms.Button btnDonateToday;
+    }
+}
---
+++ BloodBank/FinalProject/EmployeeDataDisplay.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FinalProject
+{
+    public partial class EmployeeDataDisplay : Form
+    {
+        int aPos, aNeg, bPos, bNeg, oPos, oNeg, abPos, abNeg = 0;
+
+
+
+        private void btnDonateToday_Click(object sender, EventArgs e)
+        {
+            lblOutputDonors.Text = "";
+
+            for (int i = 0; i < Donors.Count; i++)
+            {
+                DateTime date1 = Donors[i].GetDonationDate();
+                DateTime date2 = DateTime.Now;
+
+                int years = date2.Year - date1.Year;
+                int months = date2.Month - date1.Month;
+
+                if (months >= 2 || years > 0)
+                {
+                    lblOutputDonors.Text += Donors[i].GetEmail() + "\n";
+                }
+            }
+        }
+
+        List<Donor> Donors = new List<Donor>();
+
+        public EmployeeDataDisplay()
+        {
+            InitializeComponent();
+
+            ReadFile("Donors.txt", Donors);
+
+            for (int i = 0; i < Donors.Count; i++)
+            {
+                if(Donors[i].GetBloodType().ToString() == "A_POSITIVE")
+                {
+                    aPos += 1;
+                }
+                if(Donors[i].GetBloodType().ToString() == "A_NEGATIVE")
+                {
+                    aNeg += 1;
+                }
+                if (Donors[i].GetBloodType().ToString() == "B_POSITIVE")
+                {
+                    bPos += 1;
+                }
+                if (Donors[i].GetBloodType().ToString() == "B_NEGATIVE")
+                {
+                    bNeg += 1;
+                }
+                if (Donors[i].GetBloodType().ToString() == "O_POSITIVE")
+                {
+                    oPos += 1;
+                }
+                if (Donors[i].GetBloodType().ToString() == "O_NEGATIVE")
+                {
+                    oNeg += 1;
+                }
+                if (Donors[i].GetBloodType().ToString() == "AB_POSITIVE")
+                {
+                    abPos += 1;
+                }
+                if (Donors[i].GetBloodType().ToString() == "AB_NEGATIVE")
+                {
+                    abNeg += 1;
+                }
+            }
+
+            this.chtBloodTypes.Series["Blood"].Points.AddXY("A_POSITIVE", aPos);          
+            this.chtBloodTypes.Series["Blood"].Points.AddXY("A_NEGATIVE", aNeg);
+            this.chtBloodTypes.Series["Blood"].Points.AddXY("B_POSITIVE", bPos);
+            this.chtBloodTypes.Series["Blood"].Points.AddXY("B_NEGATIVE", bNeg);
+            this.chtBloodTypes.Series["Blood"].Points.AddXY("O_POSITIVE", oPos);
+            this.chtBloodTypes.Series["Blood"].Points.AddXY("O_NEGATIVE", oNeg);
+            this.chtBloodTypes.Series["Blood"].Points.AddXY("AB_POSITIVE", abPos);
+            this.chtBloodTypes.Series["Blood"].Points.AddXY("AB_NEGATIVE", abNeg);
+
+
+            var bloodTypes = new List < KeyValuePair<string, int>>();
+            bloodTypes.Add(new KeyValuePair<string, int>("A_POSITIVE", aPos));
+            bloodTypes.Add(new KeyValuePair<string, int>("A_NEGATIVE", aNeg));
+            bloodTypes.Add(new KeyValuePair<string, int>("B_POSITIVE", bPos));
+            bloodTypes.Add(new KeyValuePair<string, int>("B_NEGATIVE", bNeg));
+            bloodTypes.Add(new KeyValuePair<string, int>("O_POSITIVE", oPos));
+            bloodTypes.Add(new KeyValuePair<string, int>("O_NEGATIVE", oNeg));
+            bloodTypes.Add(new KeyValuePair<string, int>("AB_POSITIVE", abPos));
+            bloodTypes.Add(new KeyValuePair<string, int>("AB_NEGATIVE", abNeg));
+
+            bloodTypes.Sort(Compare1);
+            bloodTypes.Sort(Compare2);
+
+            lblLowBlood.Text = "Blood Shortage: " +bloodTypes[0].Key + ", " + bloodTypes[1].Key + ", & " + bloodTypes[2].Key;
+
+        }
+
+        static int Compare1(KeyValuePair<string, int> a, KeyValuePair<string, int> b)
+        {
+            return a.Key.CompareTo(b.Key);
+        }
+
+        static int Compare2(KeyValuePair<string, int> a, KeyValuePair<string, int> b)
+        {
+            return a.Value.CompareTo(b.Value);
+        }
+
+
+        private void ReadFile(string fileName, List<Donor> Donors)
+        {
+            char[] delim = { ',' };
+            int count = 0;
+            StreamReader catalogFile;
+
+            try
+            {
+                catalogFile = File.OpenText(fileName);
+                while (!catalogFile.EndOfStream)
+                {
+                    string line = catalogFile.ReadLine();
+                    string[] tokens = line.Split(delim);
+
+                    string bloodType = tokens[4];
+                    BloodType bType = (BloodType)Enum.Parse(typeof(BloodType), bloodType);
+
+                    string userDateTime = tokens[6];
+                    DateTime oDate = Convert.ToDateTime(userDateTime);
+
+                    Donors.Add(new Donor(tokens[0], tokens[1], tokens[2], tokens[3], bType, tokens[5], oDate, tokens[7], tokens[8]));
+                    count++;
+                }
+                catalogFile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Loading Error...");
+            }
+
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lblOutputDonors.Text = "";
+            string searchedDonor = txtSearchDonors.Text;
+
+            for (int i = 0; i < Donors.Count; i++)
+            {
+                if (searchedDonor == Donors[i].GetFirstName() || searchedDonor == Donors[i].GetBloodType().ToString())
+                {
+                    string donorEligibility = "No";
+
+                    DateTime date1 = Donors[i].GetDonationDate();
+                    DateTime date2 = DateTime.Now;
+
+                    int years = date2.Year - date1.Year;
+                    int months = date2.Month - date1.Month;
+
+                    if (months >= 2 || years > 0)
+                    {
+                        donorEligibility = "Yes";
+                    }
+
+                    if (Donors[i].GetInfo() == "none")
+                    {
+                        lblOutputDonors.Text += "Donor Name: " + Donors[i].GetFirstName() + " " + Donors[i].GetLastName() + "\n" +
+                            "Previous Donation: " + Donors[i].GetDonationDate().ToString("d") + "\n" +
+                            "Blood-Type: " + Donors[i].GetBloodType() + "\n" +
+                            "Address: " + Donors[i].GetAddress() + "\n" +
+                            "Email: " + Donors[i].GetEmail() + "\n" +
+                            "Eligible to donate today? " + donorEligibility + "\n\n";
+                    }
+                    else
+                    {
+                        lblOutputDonors.Text += "Donor Name: " + Donors[i].GetFirstName() + " " + Donors[i].GetLastName() + "\n" +
+                           "Previous Donation: " + Donors[i].GetDonationDate().ToString("d") + "\n" +
+                           "Blood-Type: " + Donors[i].GetBloodType() + "\n" + 
+                           "Blood-Info: " + Donors[i].GetInfo() + "\n" +
+                           "Address: " + Donors[i].GetAddress() + "\n" +
+                           "Email: " + Donors[i].GetEmail() + "\n" +
+                           "Eligible to donate today? " + donorEligibility + "\n\n";
+                    }
+                }
+            }
+
+            if(lblOutputDonors.Text == "")
+            {
+                MessageBox.Show("No Donors Found");
+            }
+        }
+
+    }
+}
