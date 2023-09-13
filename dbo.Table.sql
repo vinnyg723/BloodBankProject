ed4febfe58a864f46efa416f3ec431e74a5f6c09
FlamingTiger123 <66182847+FlamingTiger123@users.noreply.github.com>
2023-09-13 16:46:12 -0500

    Deleted previous false table created actual table for donor

--- dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[DonorId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [Address] NVARCHAR(50) NOT NULL, 
-    [PhoneNumber] NVARCHAR(50) NOT NULL
-)
